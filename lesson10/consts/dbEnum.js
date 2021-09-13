f0555a5f412e263a2b929fc1e54c06faf52184a1
Yuriy Dzyuba <iurii11022@gmail.com>
2021-09-13 18:53:45 +0300

    lesson 10

---
+++ lesson10/consts/dbEnum.js
@@ -0,0 +1,33 @@
+module.exports = {
+    CREATED_AT: 'createdAt',
+    UPDATED_AT: 'updatedAt',
+    ID: '_id',
+    V: '__v',
+
+    AUTH: 'auth',
+    ACCESS_TOKEN: 'accessToken',
+    REFRESH_TOKEN: 'refreshToken',
+
+    USER: 'user',
+    EMAIL: 'email',
+    PASSWORD: 'password',
+    NAME: 'name',
+    BORN_YEAR: 'born_year',
+    IS_ACTIVATED: 'isActivated',
+    IS_BANNED: 'isBanned',
+    ROLE: 'role',
+    AVATAR: 'avatar',
+
+    ADVERTISEMENT: 'advertisement',
+    PRODUCT: 'product',
+    PRICE: 'price',
+    IS_SALE: 'isSale',
+    QUANTITY: 'quantity',
+
+    ACTIVATE_ACCOUNT: 'activateAccount',
+    ACTIVATE_ACCOUNT_TOKEN: 'activateAccountToken',
+
+    FORGOT_PASSWORD: 'forgotPassword',
+    FORGOT_PASSWORD_TOKEN: 'forgotPasswordToken',
+
+};
